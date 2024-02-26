4b89cb998a5eb66d490f009c39bb1cbbfd94567b
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-02-27 01:30:57 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+          <div className="bg-blue-500 text-white p-4">Div 1</div>
+          <div className="bg-red-500 text-white p-4">Div 2</div>
+          <div className="bg-green-500 text-white p-4">Div 3</div>
+        </>
+      )
+    }
+
+
+    
+
+
+
+export default App
