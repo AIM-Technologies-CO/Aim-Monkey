1e9e23dec6e3a0551f18812e0c3ca5d24152dd26
Omar Naguib <55961349+OmarNaguib@users.noreply.github.com>
2024-03-03 11:45:39 +0200

    Add list styling and update template links in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { List, Modal } from 'antd';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { List, Modal } from "antd";
 
 function Home() {
   const [templates, setTemplates] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
-    console.log('fetching templates');
-    axios.get('http://localhost:3000/dev/templates')
-      .then(response => setTemplates(response.data))
-      .catch(error => {
+    console.log("fetching templates");
+    axios
+      .get("http://localhost:3000/dev/templates")
+      .then((response) => setTemplates(response.data))
+      .catch((error) => {
         console.error(error);
         setIsModalVisible(true);
       });
@@ -22,15 +23,31 @@ function Home() {
   };
 
   return (
-    <div className="grid grid-cols-2 gap-4">
+    <div className="grid grid-cols-2 gap-4 justify-items-center items-center">
       <h1 className="col-span-2">Available Templates</h1>
-      {templates.map(template => (
-        <List.Item key={template}>
-          <Link to={`/template/${template}`}>{template}</Link>
+      {templates.map((template) => (
+        <List.Item
+          key={template}
+          className="p-4 border-b border-gray-200 bg-blue-500 mb-4 w-[400px]"
+        >
+          <Link
+            to={`/template/${template}`}
+            className="text-white hover:text-blue-700 font-bold"
+          >
+            {template}
+          </Link>
         </List.Item>
       ))}
-      <Modal title="Request failed" open={isModalVisible} onOk={handleOk} onCancel={handleOk}>
-        <p>Make sure you have Aim-Pdf-Service running locally with the "report-devtool" branch</p>
+      <Modal
+        title="Request failed"
+        open={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleOk}
+      >
+        <p>
+          Make sure you have Aim-Pdf-Service running locally with the
+          "report-devtool" branch
+        </p>
       </Modal>
     </div>
   );
